6824005a56daf4cd37a033fb8256b99299cc6853
Yu Xie <xieyu.fish@gmail.com>
2021-11-02 15:55:10 -0400

    merge master python tests

--- tests/get_sgp.py
+++ tests/get_sgp.py
@@ -1,5 +1,5 @@
 import numpy as np
-from flare_pp._C_flare import SparseGP, NormalizedDotProduct, B2, Structure
+from flare_pp._C_flare import SparseGP, NormalizedDotProduct, Bk, Structure
 from flare_pp.sparse_gp import SGP_Wrapper
 from flare_pp.sparse_gp_calculator import SGP_Calculator
 from flare.ase.atoms import FLARE_Atoms
@@ -8,31 +8,46 @@ from ase.calculators.lj import LennardJones
 from ase.build import make_supercell
 
 
-# Define kernel.
+# Define kernels.
+sigma = 1.0
+power = 2
+kernel1 = NormalizedDotProduct(sigma, power)
+
 sigma = 2.0
 power = 2
-kernel = NormalizedDotProduct(sigma, power)
+kernel2 = NormalizedDotProduct(sigma, power)
 
-# Define B2 calculator.
+sigma = 3.0
+power = 2
+kernel3 = NormalizedDotProduct(sigma, power)
+
+# Define calculators.
+species_map = {6: 0, 8: 1}
+
 cutoff = 5.0
 cutoff_function = "quadratic"
 radial_basis = "chebyshev"
 radial_hyps = [0.0, cutoff]
 cutoff_hyps = []
-descriptor_settings = [2, 8, 3]
-b2_calc = B2(radial_basis, cutoff_function, radial_hyps, cutoff_hyps,
-             descriptor_settings)
+
+settings = [len(species_map), 1, 4, 0]
+calc1 = Bk(radial_basis, cutoff_function, radial_hyps, cutoff_hyps, settings)
+
+settings = [len(species_map), 2, 4, 3]
+calc2 = Bk(radial_basis, cutoff_function, radial_hyps, cutoff_hyps, settings)
+
+settings = [len(species_map), 3, 2, 2]
+calc3 = Bk(radial_basis, cutoff_function, radial_hyps, cutoff_hyps, settings)
 
 # Define remaining parameters for the SGP wrapper.
 sigma_e = 0.001
 sigma_f = 0.05
 sigma_s = 0.006
-species_map = {6: 0, 8: 1}
 single_atom_energies = {0: -5, 1: -6}
 variance_type = "local"
 max_iterations = 20
 opt_method = "L-BFGS-B"
-bounds = [(None, None), (sigma_e, None), (None, None), (None, None)]
+bounds = [(None, None), (None, None), (None, None), (sigma_e, None), (None, None), (None, None)]
 
 
 def get_random_atoms(a=2.0, sc_size=2, numbers=[6, 8],
@@ -57,9 +72,18 @@ def get_random_atoms(a=2.0, sc_size=2, numbers=[6, 8],
 
 def get_empty_sgp():
     empty_sgp = SGP_Wrapper(
-        [kernel], [b2_calc], cutoff, sigma_e, sigma_f, sigma_s, species_map,
-        single_atom_energies=single_atom_energies, variance_type=variance_type,
-        opt_method=opt_method, bounds=bounds, max_iterations=max_iterations
+        [kernel1, kernel2, kernel3], 
+        [calc1, calc2, calc3], 
+        cutoff, 
+        sigma_e, 
+        sigma_f, 
+        sigma_s, 
+        species_map,
+        single_atom_energies=single_atom_energies, 
+        variance_type=variance_type,
+        opt_method=opt_method, 
+        bounds=bounds, 
+        max_iterations=max_iterations,
     )
 
     return empty_sgp
@@ -74,8 +98,14 @@ def get_updated_sgp():
     stress = training_structure.get_stress()
 
     sgp = get_empty_sgp()
-    sgp.update_db(training_structure, forces, custom_range=(1, 2, 3, 4, 5),
-                  energy=energy, stress=stress, mode="specific")
+    sgp.update_db(
+        training_structure, 
+        forces, 
+        custom_range=(1, 2, 3, 4, 5),
+        energy=energy, 
+        stress=stress, 
+        mode="specific",
+    )
 
     return sgp
 
